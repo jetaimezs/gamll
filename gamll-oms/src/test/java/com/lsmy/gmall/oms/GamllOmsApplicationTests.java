ba205d06cab991d6d95901cfd791d669fcef40c8
jetaimezs <454666870@qq.com>
2020-11-18 11:22:57 +0800

    结构

---
+++ gamll-oms/src/test/java/com/lsmy/gmall/oms/GamllOmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsmy.gmall.oms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamllOmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ gmall-admin-web/src/test/java/com/lsmy/gmall/admin/GmallAdminWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsmy.gmall.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallAdminWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
